5b4818af8943ce31d3e1facab4c19c5846855e63
hekang1992 <hekang19921111@hotmail.com>
2024-03-25 12:02:55 +0800

    add:集成cocoapods

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '12.0'
+
+target 'TaraLoan' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TaraLoan
+
+  target 'TaraLoanTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TaraLoanUITests' do
+    # Pods for testing
+  end
+
+end
--- TaraLoan/Main/AppDelegate.swift
+++ TaraLoan/Main/AppDelegate.swift
@@ -10,15 +10,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        window?.rootViewController = LaunchViewController()
+        window?.makeKeyAndVisible()
         return true
     }
-
     
-
-
 }
 

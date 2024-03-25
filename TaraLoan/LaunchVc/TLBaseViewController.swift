0b19cbefb6787a88e281bbd31fd89c216ee9957e
hekang1992 <hekang19921111@hotmail.com>
2024-03-25 11:58:17 +0800

    first commit

---
+++ TaraLoan/LaunchVc/TLBaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TLBaseViewController.swift
+//  TaraLoan
+//
+//  Created by apple on 2024/3/25.
+//
+
+import UIKit
+
+class TLBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TaraLoan/Main/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  TaraLoan
+//
+//  Created by apple on 2024/3/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+

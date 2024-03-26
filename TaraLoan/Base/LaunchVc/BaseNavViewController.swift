790c5b8c87c198f295f49876d7c2116ea87da529
hekang1992 <hekang19921111@hotmail.com>
2024-03-26 17:53:34 +0800

    ref:优化

--- TaraLoan/Base/LaunchVc/BaseNavViewController.swift
+++ TaraLoan/Base/LaunchVc/BaseNavViewController.swift
@@ -11,8 +11,16 @@ class BaseNavViewController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        self.navigationBar.isHidden = true
+        self.navigationBar.isTranslucent = false
+    }
+    
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        if self.viewControllers.count > 0 {
+            viewController.hidesBottomBarWhenPushed = true
+        }
+        super.pushViewController(viewController, animated: animated)
     }
     
 
